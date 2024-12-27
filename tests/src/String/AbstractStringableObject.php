f33eb499aaed7d81183e170fe9c11769c1250fe2
Gabriel Tenita <the.ge@tenita.eu>
2024-12-27 15:07:02 +0200

    Added tests

---
+++ tests/src/String/AbstractStringableObject.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+/**
+ * [TODO] add description
+ *
+ * File name: AbstractStringableObject.php
+ * Created:   2024-12-10 17:25:27
+ * @author    Gabriel Tenita <dev2023@tenita.eu>
+ * @link      https://github.com/the-ge/
+ * @copyright Copyright (c) 2024-present Gabriel Tenita
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License version 2.0
+ */
+
+namespace TheGe\XtraTesting;
+
+use Stringable;
+use TheGe\Xtra\String\StringableObjectMethods;
+
+
+abstract class AbstractStringableObject implements Stringable
+{
+    use StringableObjectMethods;
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'the-ge/php-xtra',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1e3d8106c1abd78286526b6260ad64cd11c19957',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpstan/phpstan' => array(
+            'pretty_version' => '2.0.4',
+            'version' => '2.0.4.0',
+            'reference' => '50d276fc3bf1430ec315f2f109bbde2769821524',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+        'the-ge/php-xtra' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1e3d8106c1abd78286526b6260ad64cd11c19957',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
